5a45d1bf2b44a06393a136cd3dcb83d707696f1e
epodberezko <epodberezko@phoenixit.ru>
2023-02-15 23:26:02 +0300

    feat: тест роутинга

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Counter } from "./components";
+import React, { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss'
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { CounterPageAsync } from "./pages/CounterPage/CounterPage.async";
 
 const App = () => {
   return (
     <div className={'app'}>
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Link to={'/counter'}>Счётчик</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPageAsync />}/>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/counter'} element={<CounterPageAsync />}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/CounterPage/CounterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Counter } from "../../components";
+
+const CounterPage = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default CounterPage;
