b1db480dd400ab439f48c23420b20d38265daf6d
epodberezko <epodberezko@phoenixit.ru>
2023-03-05 14:03:14 +0300

    fix: поправлен импорт классов

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,7 +3,7 @@ type Mods = Record<string, boolean | string>
 export function classNames(cls: string, mods: Mods, additional: string[]) {
   return [
     cls,
-    Object.entries(mods).filter(([key, value]) => Boolean(value)).map(([key, value]) => key),
+    Object.entries(mods).filter(([, value]) => Boolean(value)).map(([key]) => key),
     ...additional,
   ].join(' ');
 }
