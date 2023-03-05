bcb7d93de58919a68349dd8c750079c8eda93aa3
epodberezko <epodberezko@phoenixit.ru>
2023-03-05 19:24:52 +0300

    refactor: создан конфиг роутов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/index.scss'
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "app/providers/router/undex";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
@@ -14,12 +13,7 @@ const App = () => {
       <Link className={'link'} to={'/'}>Главная</Link>
       <Link className={'link'} to={'/about'}>О сайте</Link>
       <button onClick={toggleTheme}>Сменить тему</button>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/'} element={<MainPage />}/>
-          <Route path={'/about'} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
