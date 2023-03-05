5040bbeee9a78ba75aaa9b72f8c30fd22cdee3b8
epodberezko <epodberezko@phoenixit.ru>
2023-03-05 21:18:58 +0300

    feat: создан компонент навбара

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './styles/index.scss'
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router/undex";
+import { Navbar } from "widgets/Navbar/indext";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link className={'link'} to={'/'}>Главная</Link>
-      <Link className={'link'} to={'/about'}>О сайте</Link>
+      <Navbar />
       <button onClick={toggleTheme}>Сменить тему</button>
       <AppRouter />
     </div>
