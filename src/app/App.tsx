61056a5230e306b543717ac898f84d5db8aa5c0e
epodberezko <epodberezko@phoenixit.ru>
2023-03-12 00:43:19 +0300

    refactor: переключатель темы перенесён в навбар

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,11 @@ import { AppRouter } from "app/providers/router/undex";
 import { Navbar } from "widgets/Navbar/indext";
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>Сменить тему</button>
       <AppRouter />
     </div>
   );
