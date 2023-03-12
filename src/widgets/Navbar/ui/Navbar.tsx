a6b6397b5007a2576681f903228c6acfea41e87d
epodberezko <epodberezko@phoenixit.ru>
2023-03-12 22:17:54 +0300

    feat: добавлена интернационализация + компонент переключения языка

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { classNames } from "shared/lib";
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from "shared/ui";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const {t} = useTranslation();
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-          Main
+          {t('Главная')}
         </AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-          About
+          {t('О сайте')}
         </AppLink>
       </div>
 
