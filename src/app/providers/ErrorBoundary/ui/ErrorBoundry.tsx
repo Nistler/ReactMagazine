55eabd0dcf91fee3b0e01ce1ef2351d711f40f7b
epodberezko <epodberezko@phoenixit.ru>
2023-03-15 22:58:51 +0300

    feat(error-boundary): реализован error boundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundry.tsx
@@ -0,0 +1,40 @@
+import type { ErrorInfo, ReactNode } from 'react';
+import React, { Suspense } from 'react';
+import { PageError } from 'widgets/PageError';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    ErrorBoundary._error = error;
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return <Suspense fallback=""><PageError /></Suspense>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
