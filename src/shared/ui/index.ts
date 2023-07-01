de51ec0cc7564b54ff5ccc7164d9b60e0e9686e3
Nistler <evgensin@gmail.com>
2023-07-01 21:50:03 +0300

    feat(Input): реализован кастомный инпут

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,6 +2,7 @@ import { Loader } from 'shared/ui/Loader/Loader';
 import { Button, ButtonTheme } from './Button/Button';
 import { AppLink, AppLinkTheme } from './AppLink/AppLink';
 import { Modal } from './Modal/Modal';
+import { Input } from './Input/Input';
 
 export {
   Button,
@@ -10,4 +11,5 @@ export {
   AppLinkTheme,
   Loader,
   Modal,
+  Input,
 };
